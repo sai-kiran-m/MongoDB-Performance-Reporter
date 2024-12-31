d03b9031fe79d2aa18cd03a67ab5397a641bbb70
sai-kiran-m <i.sai.kiran04@gmail.com>
2024-12-31 10:34:09 -0500

    Use logger

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from reports.factories.recommend_indices_factory import RecommendIndicesFactory
 from reports.factories.report_factory import ReportFactoryBase
 from reports.report_base import ReportBase
 from shared.utils.get_config import Config
+from shared.utils.logger import get_logger
 
+logger = get_logger(__name__)
 FACTORY_MAP = {"recommend_indices": RecommendIndicesFactory}
 
 parser = argparse.ArgumentParser(
@@ -16,7 +18,9 @@ parser.add_argument("--report_name", help="Provide the name of report")
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    logger.info(f"Recieved input args {args.__dict__}")
     report_name = args.report_name
+
     config: Config = Config()
     report_factory: ReportFactoryBase = FACTORY_MAP[report_name](config)
     report: ReportBase = report_factory.create_report()
